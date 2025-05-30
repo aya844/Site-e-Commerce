0e8d1eb5cba8bea227995ed816918d6b80ea8288
meryemk <meryem.karoui@insat.ucar.tn>
2025-05-30 14:10:36 +0100

    Export de la BD apres ajout de table d'historique de stock

---
+++ my_shop_dev1.sql
@@ -0,0 +1,333 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 30 mai 2025 à 15:01
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `my_shop_dev1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ajouterhistoriqueproduit`
+--
+
+CREATE TABLE `ajouterhistoriqueproduit` (
+  `id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `ajouterhistoriqueproduit`
+--
+
+INSERT INTO `ajouterhistoriqueproduit` (`id`, `produit_id`, `quantite`, `created_at`) VALUES
+(1, 6, 100, '2025-05-30 11:28:09'),
+(2, 2, 100, '2025-05-30 12:47:15'),
+(3, 2, 100, '2025-05-30 13:01:13'),
+(4, 2, 100, '2025-05-30 13:01:28'),
+(5, 2, 100, '2025-05-30 13:02:33'),
+(6, 2, 120, '2025-05-30 13:07:15'),
+(7, 2, 100, '2025-05-30 13:11:26'),
+(8, 6, 30, '2025-05-30 13:11:37'),
+(9, 4, 30, '2025-05-30 13:42:16'),
+(10, 5, 60, '2025-05-30 13:47:18'),
+(11, 4, 500, '2025-05-30 14:42:40');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+(2, 'flower22'),
+(4, 'daisy'),
+(5, 'electronique'),
+(6, 'electronique5'),
+(7, 'iuyy');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20250524221128', '2025-05-25 00:13:14', 80),
+('DoctrineMigrations\\Version20250524230751', '2025-05-25 01:08:03', 17),
+('DoctrineMigrations\\Version20250529085042', '2025-05-29 10:52:14', 54),
+('DoctrineMigrations\\Version20250529125533', '2025-05-29 14:56:15', 136),
+('DoctrineMigrations\\Version20250529142330', '2025-05-29 16:24:56', 7),
+('DoctrineMigrations\\Version20250529145831', '2025-05-29 16:58:59', 74),
+('DoctrineMigrations\\Version20250529185326', '2025-05-29 22:36:55', 36),
+('DoctrineMigrations\\Version20250529203642', '2025-05-30 10:42:59', 8),
+('DoctrineMigrations\\Version20250529220529', '2025-05-30 10:42:59', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+
+CREATE TABLE `produits` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` longtext DEFAULT NULL,
+  `prix` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT INTO `produits` (`id`, `nom`, `description`, `prix`, `image`, `stock`) VALUES
+(2, 'iphone', 'blablabla', 4000, NULL, 100),
+(4, 'iphone', 'blablabla', 4000, NULL, 530),
+(5, 'plante', 'tahfouna', 1000000, NULL, 60),
+(6, 'llllll', NULL, 20, 'meryouma-68397a296017c.jpg', 130);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits_sous_categorie`
+--
+
+CREATE TABLE `produits_sous_categorie` (
+  `produits_id` int(11) NOT NULL,
+  `sous_categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `produits_sous_categorie`
+--
+
+INSERT INTO `produits_sous_categorie` (`produits_id`, `sous_categorie_id`) VALUES
+(2, 2),
+(4, 2),
+(5, 2),
+(6, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sous_categorie`
+--
+
+CREATE TABLE `sous_categorie` (
+  `id` int(11) NOT NULL,
+  `categorie_id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `sous_categorie`
+--
+
+INSERT INTO `sous_categorie` (`id`, `categorie_id`, `nom`) VALUES
+(2, 2, 'uuuuu');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:json)' CHECK (json_valid(`roles`)),
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `first_name`, `last_name`) VALUES
+(4, 'aya@gmail.com', '[\"ROLE_ADMIN\", \"ROLE_EDITOR\", \"ROLE_USER\"]', '$2y$13$Y6JdmFylIBsxJeTWoc5HLe/nBfbYE81xZlUZGoia38OICWp82M00W', 'aya', 'aya'),
+(5, 'meriemkaroui55@gmail.com', '[\"ROLE_ADMIN\", \"ROLE_EDITOR\", \"ROLE_USER\"]', '$2y$13$x4n7Bx09KFV8Q7SB1SRvee6NaXk4T7HGu2cbj8QtTotwebJ1ocIZC', 'mery', 'karoui');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `ajouterhistoriqueproduit`
+--
+ALTER TABLE `ajouterhistoriqueproduit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_E3E57862F347EFB` (`produit_id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `produits_sous_categorie`
+--
+ALTER TABLE `produits_sous_categorie`
+  ADD PRIMARY KEY (`produits_id`,`sous_categorie_id`),
+  ADD KEY `IDX_1E1E0853CD11A2CF` (`produits_id`),
+  ADD KEY `IDX_1E1E0853365BF48` (`sous_categorie_id`);
+
+--
+-- Index pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_52743D7BBCF5E72D` (`categorie_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `ajouterhistoriqueproduit`
+--
+ALTER TABLE `ajouterhistoriqueproduit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `ajouterhistoriqueproduit`
+--
+ALTER TABLE `ajouterhistoriqueproduit`
+  ADD CONSTRAINT `FK_E3E57862F347EFB` FOREIGN KEY (`produit_id`) REFERENCES `produits` (`id`);
+
+--
+-- Contraintes pour la table `produits_sous_categorie`
+--
+ALTER TABLE `produits_sous_categorie`
+  ADD CONSTRAINT `FK_1E1E0853365BF48` FOREIGN KEY (`sous_categorie_id`) REFERENCES `sous_categorie` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_1E1E0853CD11A2CF` FOREIGN KEY (`produits_id`) REFERENCES `produits` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  ADD CONSTRAINT `FK_52743D7BBCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
